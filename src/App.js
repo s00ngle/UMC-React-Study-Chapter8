89d491c384614a27f31e3c3c3c14d1fe59b30a25
Soongle <dydtns9815@naver.com>
2023-05-31 10:40:56 +0900

    year 변경 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-const year = 2023;
 const pageNo = 1;
 const numOfRows = 1000;
 const serviceKey = process.env.REACT_APP_API_KEY;
-
-const URL = `https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo?year=${year}&pageNo=${pageNo}&numOfRows=${numOfRows}&returnType=json&serviceKey=${serviceKey}`;
 
 function App() {
     const [data, setData] = useState([]);
     const [filterName, setFilterName] = useState("");
     const [filteredData, setFilteredData] = useState([]);
+    const [year, setYear] = useState(2023);
+
+    const URL = `https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo?year=${year}&pageNo=${pageNo}&numOfRows=${numOfRows}&returnType=json&serviceKey=${serviceKey}`;
 
     const fetchData = async () => {
         try {
@@ -32,7 +32,7 @@ function App() {
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [year]);
 
     const setFilter = (e) => {
         setFilterName(e.target.value);
@@ -54,22 +54,36 @@ function App() {
                     }}
                     placeholder="Enter filter name"
                 />
+                <input
+                    type="text"
+                    value={year}
+                    onChange={(e) => {
+                        setYear(e.target.value);
+                    }}
+                    placeholder="Enter year"
+                />
             </div>
 
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-                {filteredData.map((item, index) => (
-                    <div className="item" key={index} style={{ width: "15%" }}>
-                        <div className="districtName">
-                            지역 : {item.districtName}
+            {year >= 2018 && (
+                <div style={{ display: "flex", flexWrap: "wrap" }}>
+                    {filteredData.map((item, index) => (
+                        <div
+                            className="item"
+                            key={index}
+                            style={{ width: "15%" }}
+                        >
+                            <div className="districtName">
+                                지역 : {item.districtName}
+                            </div>
+                            <hr />
+                            <div>경보단계 : {item.issueGbn}</div>
+                            <div>발령일 : {item.issueDate}</div>
+                            <div>해제일 : {item.clearDate}</div>
+                            <div>해제농도 : {item.clearVal}</div>
                         </div>
-                        <hr />
-                        <div>경보단계 : {item.issueGbn}</div>
-                        <div>발령일 : {item.issueDate}</div>
-                        <div>해제일 : {item.clearDate}</div>
-                        <div>해제농도 : {item.clearVal}</div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
